d6f9417f29a182aa3b9daf597cda874ba0ec3854
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 17:29:30 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TP1_EJ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Title = "Distancia de KM a millas y yardas ";
+
+            double Kilometros, Millas, Yardas;
+
+            Console.Write("Ingrese la cantidad de Kilometros: ");
+            Kilometros = Convert.ToSingle(Console.ReadLine());
+
+            Yardas = Kilometros * 1093.6133;
+            Millas = Kilometros * 0.621371192;
+
+            Console.WriteLine($"{Kilometros} Kilometros son {Millas} Millas, y  {Yardas} Yardas");
+
+
+
+        }
+    }
+}
